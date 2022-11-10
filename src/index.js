11db4fe3305309cf3588421e06bdd9be3fff077a
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-09 22:25:09 -0300

    docs: gramatica do nosso linguagem

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * expr => term oper
+ *
+ * oper => + term { print('+') } oper
+ *      |  - term { print('-') } oper
+ *      |  term
+ *
+ * term => 0 { print('0') }
+ *      |  1 { print('1') }
+ *      |  ...
+ *      |  9 { print('9') }
+ */
