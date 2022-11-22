45c8d09959e62d425e37f964c37c9a2dce0a3561
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-22 07:06:26 -0300

    chore: vou trabalhar com o loader es module

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+}
