30decd87e5f447ba37847008e95d112266146d22
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-24 09:41:44 -0300

    test(h): separando alguns testes em cases diferentes

--- standard/h/src/__tests__/h.test.js
+++ standard/h/src/__tests__/h.test.js
@@ -1,15 +1,25 @@
 import h from '../h'
 
 describe('standard.h', () => {
-  it('Deve criar um elemento html quando a funcao h for executado', () => {
-    const element = h('div')
+  let attrs
+  let element
+  let tagName
 
-    expect(element).toBeInstanceOf(HTMLElement)
-    expect(element.nodeName).toBe('DIV')
+  beforeEach(() => {
+    tagName = 'DIV'
+    attrs = { id: 'kuba' }
+    element = h(tagName, attrs)
   })
 
-  it('Deve criar um elemento html com seus atributos quando a funcao h for executado', () => {
-    const element = h('div', { id: 'kuba' })
-    expect(element.getAttribute('id')).toBe('kuba')
+  it('Deve criar um leemento do tipo HTMLElement', () => {
+    expect(element).toBeInstanceOf(HTMLElement)
+  })
+
+  it('Deve criar um elemento com o nodeName DIV', () => {
+    expect(element.nodeName).toBe(tagName)
+  })
+
+  it('Deve criar o atributo id com o valor kuba', () => {
+    expect(element.getAttribute('id')).toBe(attrs.id)
   })
 })
