0723941c339fdaf20fe669617231ba04cc763261
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-24 09:34:13 -0300

    test(h): deve criar um elemento html com seus atributos quando a funcao h form executado

--- standard/h/src/__tests__/h.test.js
+++ standard/h/src/__tests__/h.test.js
@@ -7,4 +7,9 @@ describe('standard.h', () => {
     expect(element).toBeInstanceOf(HTMLElement)
     expect(element.nodeName).toBe('DIV')
   })
+
+  it('Deve criar um elemento html com seus atributos quando a funcao h for executado', () => {
+    const element = h('div', { id: 'kuba' })
+    expect(element.getAttribute('id')).toBe('kuba')
+  })
 })
--- standard/h/src/h.js
+++ standard/h/src/h.js
@@ -1,3 +1,9 @@
-export default (tagName) => (
-  document.createElement(tagName)
-)
+export default (tagName, attrs = {}) => {
+  const element = document.createElement(tagName)
+
+  Object
+    .entries(attrs)
+    .forEach(([key, value]) => element.setAttribute(key, value))
+
+  return element
+}
