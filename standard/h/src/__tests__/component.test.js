15c07814c19b3662bf1fc8d64d0d78187fcb6ab8
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-25 11:45:42 -0300

    test(h.component): deve retornar truthy quando o nodeName for uma function

--- standard/h/src/__tests__/component.test.js
+++ standard/h/src/__tests__/component.test.js
@@ -1,4 +1,15 @@
+import Component from '../component'
+
 describe('standard.h.component', () => {
-  test.todo('Deve retornar truthy quando o nodeName for uma function')
+  let nodeName
+
+  beforeEach(() => {
+    nodeName = jest.fn()
+  })
+
+  test('Deve retornar truthy quando o nodeName for uma function', () => {
+    expect(Component.is(nodeName)).toBeTruthy()
+  })
+
   test.todo('Deve executar o component quando o metodo execute for executado')
 })
