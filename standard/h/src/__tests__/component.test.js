8c9467cd5f3812e7098f3bcb5236cebd5cea05f2
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-25 08:04:24 -0300

    test(h.component): todos de testes para serem desenvolvidos

---
+++ standard/h/src/__tests__/component.test.js
@@ -0,0 +1,4 @@
+describe('standard.h.component', () => {
+  test.todo('Deve retornar truthy quando o nodeName for uma function')
+  test.todo('Deve executar o component quando o metodo execute for executado')
+})
