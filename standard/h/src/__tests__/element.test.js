c1c5a584f82b1070c153856abe2f2cee2876eddc
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-24 22:02:43 -0300

    test(h.element): deve criar um elemento quando o metodo create for executado

--- standard/h/src/__tests__/element.test.js
+++ standard/h/src/__tests__/element.test.js
@@ -1,3 +1,4 @@
 describe('standard.h.element', () => {
   test.todo('Deve retornar truthy quando o nodeName for uma string')
+  test.todo('Deve criar um elemento quando o metodo create for executado')
 })
