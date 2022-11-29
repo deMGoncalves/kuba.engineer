c465fb366b6d885de44f5e76c8b60d9424de4f0b
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-29 09:25:28 -0300

    test(h.render): apenda o HTMLElement retornado do contrato render.paint do element

--- standard/h/src/__tests__/render.test.js
+++ standard/h/src/__tests__/render.test.js
@@ -2,15 +2,10 @@ import render from '../render'
 
 describe('standard.h.render', () => {
   let element
-  let node
 
   beforeEach(() => {
     element = {
       [render.paint]: jest.fn().mockReturnValue(HTMLElement)
-    }
-
-    node = {
-      append: jest.fn()
     }
   })
 
@@ -19,11 +14,13 @@ describe('standard.h.render', () => {
   })
 
   test('Apenda o HTMLElement retornado do contrato render.paint no element', () => {
-    render(node, element)
+    const append = jest.spyOn(document.body, 'append')
+
+    render(document.body, element)
 
     expect(element[render.paint]).toHaveBeenCalledTimes(1)
 
-    expect(node.append).toHaveBeenCalledTimes(1)
-    expect(node.append).toHaveBeenCalledWith(HTMLElement)
+    expect(append).toHaveBeenCalledTimes(1)
+    expect(append).toHaveBeenCalledWith(HTMLElement)
   })
 })
