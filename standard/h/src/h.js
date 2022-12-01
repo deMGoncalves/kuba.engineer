3ec859949172097d856555d5de62f1e3d28cbaad
deMGoncalves <cleber.engineer@hotmail.com>
2022-11-30 22:59:46 -0300

    fix(h): assinatura do h permite que o attrs seje null

--- standard/h/src/h.js
+++ standard/h/src/h.js
@@ -2,6 +2,7 @@ import Component from './component'
 import Element from './element'
 
 export default (nodeName, attrs, ...children) => {
+  attrs ??= {}
   if (Element.is(nodeName)) return Element.create(nodeName, attrs, children)
   if (Component.is(nodeName)) return Component.execute(nodeName, attrs, children)
 }
