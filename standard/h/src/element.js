ef4662200d9f8fbe813f572c9c872a1d32b145fe
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-02 13:49:10 -0300

    chore(h.element): fim do flow de renderizacao e contrato do reflow

--- standard/h/src/element.js
+++ standard/h/src/element.js
@@ -1,13 +1,18 @@
 import Attributes from './attributes'
 import Children from './children'
 import ClassName from './className'
+import Events from './events'
+import Is from './is'
 import overload from '@kuba/overload'
 import render from './render'
+import repaint from './repaint'
 
 class Element {
   #attributes
   #children
   #className
+  #events
+  #is
   #node
   #nodeName
 
@@ -16,22 +21,30 @@ class Element {
     this.#attributes = Attributes.create(attrs, this)
     this.#children = Children.create(children, this)
     this.#className = ClassName.create(attrs, this)
+    this.#events = Events.create(attrs, this)
+    this.#is = Is.create(attrs)
   }
 
   @overload(
     Attributes.target,
-    ClassName.target
+    ClassName.target,
+    Events.target
   )
   [Children.parent] () {
     return this.#node
   }
 
-  [render.paint] () {
-    this.#node ??= document.createElement(this.#nodeName)
-    this.#attributes[render.paint]()
-    this.#className[render.paint]()
-    this.#children[render.paint]()
+  [render.flow] () {
+    this.#node ??= document.createElement(this.#nodeName, { is: this.#is })
+    this.#events[render.flow]()
+    this.#attributes[render.flow]()
+    this.#className[render.flow]()
+    this.#children[render.flow]()
     return this.#node
+  }
+
+  [repaint.reflow] (_ast) {
+    return this
   }
 
   static create (nodeName, attrs, children) {
