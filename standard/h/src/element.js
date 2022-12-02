229df5f2d45eccf56c28da9ec0d8146276cc3f9e
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-02 08:05:05 -0300

    chore(h.element): inclusao do className

--- standard/h/src/element.js
+++ standard/h/src/element.js
@@ -1,11 +1,13 @@
 import Attributes from './attributes'
 import Children from './children'
+import ClassName from './className'
 import overload from '@kuba/overload'
 import render from './render'
 
 class Element {
   #attributes
   #children
+  #className
   #node
   #nodeName
 
@@ -13,10 +15,12 @@ class Element {
     this.#nodeName = nodeName
     this.#attributes = Attributes.create(attrs, this)
     this.#children = Children.create(children, this)
+    this.#className = ClassName.create(attrs, this)
   }
 
   @overload(
-    Attributes.target
+    Attributes.target,
+    ClassName.target
   )
   [Children.parent] () {
     return this.#node
@@ -25,6 +29,7 @@ class Element {
   [render.paint] () {
     this.#node ??= document.createElement(this.#nodeName)
     this.#attributes[render.paint]()
+    this.#className[render.paint]()
     this.#children[render.paint]()
     return this.#node
   }
