c2b0f75789208a6fdd3fdafa091966874d324a93
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-02 13:42:00 -0300

    chore(eslint): permitindo o encadeamento de instrucoes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ module.exports = {
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error'
+    'jest/valid-expect': 'error',
+    'no-sequences': 'off'
   }
 }
