8443f12ea1477f4011c6cc26b42d46655b55cf0b
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-03 11:08:07 -0300

    chore: implementando um contador

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import h, { render } from '@kuba/h'
+import h, { Fragment, paint, render, repaint } from '@kuba/h'
+
+const component = (props) => (
+  <>
+    <button onClick={() => props.onClick()}>+</button>
+    <div>{props.value}</div>
+  </>
+)
+
+@paint(component)
+class Contador {
+  #value = 0
+
+  get value () {
+    return this.#value
+  }
+
+  @repaint
+  onClick () {
+    this.#value += 1
+    console.log(this.#value)
+    return this
+  }
+}
 
 render(
   document.body,
-  <h1>Kuba</h1>
+  <Contador />
 )
