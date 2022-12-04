7b85018021728a7a6a07c9313a4997179fcbb1c9
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-04 20:39:04 -0300

    test(magic): retorna  a mesma referencia uma vez que o simbolo tenha cido criado

--- standard/magic/src/__tests__/magic.test.js
+++ standard/magic/src/__tests__/magic.test.js
@@ -1,3 +1,4 @@
 describe('standard.magic', () => {
   test.todo('Cria um simbolo usando o key como identificador')
+  test.todo('Retorna a mesma referencia uma vez que o simbolo tenha cido criado')
 })
