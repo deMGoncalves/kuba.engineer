1adef9b7ba42dbeec8dfae6058274853499789f8
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-04 20:30:46 -0300

    test(overload): parametros dos metodos (X, Y e Z) sera o mesmo metodo A

--- standard/overload/src/__tests__/overload.test.js
+++ standard/overload/src/__tests__/overload.test.js
@@ -12,15 +12,31 @@ describe('standard.overload', () => {
     }
   }
 
-  test('Executa o metodo A quando os metodos apendados (X, Y e Z) forem executados', () => {
-    const methodA = jest.spyOn(Sparring.prototype, 'methodA')
-    const sparring = new Sparring()
+  let methodA
+  let sparring
 
+  beforeEach(() => {
+    methodA = jest.spyOn(Sparring.prototype, 'methodA')
+    sparring = new Sparring()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Executa o metodo A quando os metodos apendados (X, Y e Z) forem executados', () => {
     sparring.methodX()
     sparring.methodY()
     sparring.methodZ()
     sparring.methodA()
 
     expect(methodA).toHaveBeenCalledTimes(4)
+  })
+
+  test('Parametros dos metodos (X, Y e Z) sera o mesmo para o metodo A', () => {
+    sparring.methodX(1, 2, 3)
+
+    expect(methodA).toHaveBeenCalledTimes(1)
+    expect(methodA).toHaveBeenCalledWith(1, 2, 3)
   })
 })
