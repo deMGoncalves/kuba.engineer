73fb1fe8ae3208250c0a94f96b963d0089b04535
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-04 21:00:47 -0300

    test(overload): retorna a mesma referencia uma vez que o simbolo tenh cido criado

--- standard/magic/src/__tests__/magic.test.js
+++ standard/magic/src/__tests__/magic.test.js
@@ -1,4 +1,15 @@
+import magic from '../magic'
+
 describe('standard.magic', () => {
+  let dunder
+
+  beforeEach(() => {
+    dunder = magic.__method__
+  })
+
   test.todo('Cria um simbolo usando o key como identificador')
-  test.todo('Retorna a mesma referencia uma vez que o simbolo tenha cido criado')
+
+  test('Retorna a mesma referencia uma vez que o simbolo tenha cido criado', () => {
+    expect(dunder).toBe(magic.__method__)
+  })
 })
