c5d56a28066c49a62dcf27cb29438b147cc5feac
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-04 21:04:02 -0300

    test(magic): cria um simbolo usando o key como identificador

--- standard/magic/src/__tests__/magic.test.js
+++ standard/magic/src/__tests__/magic.test.js
@@ -2,12 +2,17 @@ import magic from '../magic'
 
 describe('standard.magic', () => {
   let dunder
+  let symbol
 
   beforeEach(() => {
+    symbol = jest.spyOn(window, 'Symbol')
     dunder = magic.__method__
   })
 
-  test.todo('Cria um simbolo usando o key como identificador')
+  test('Cria um simbolo usando o key como identificador', () => {
+    expect(symbol).toHaveBeenCalledTimes(1)
+    expect(symbol).toHaveBeenCalledWith('__method__')
+  })
 
   test('Retorna a mesma referencia uma vez que o simbolo tenha cido criado', () => {
     expect(dunder).toBe(magic.__method__)
