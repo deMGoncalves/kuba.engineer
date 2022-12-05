7b0a147c1b3adcaad4f4f5c09ff378e8d44a69ff
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-05 20:09:33 -0300

    chore(h.reflow): definicao dos contratos para o processo de zip

--- standard/h/src/reflow.js
+++ standard/h/src/reflow.js
@@ -1,5 +1,21 @@
-class Reflow {
+import magic from '@kuba/magic'
 
+class Reflow {
+  static get add () {
+    return magic.__add__
+  }
+
+  static get remove () {
+    return magic.__remove__
+  }
+
+  static get replace () {
+    return magic.__replace__
+  }
+
+  static zip (_child, _nChild) {
+    return undefined
+  }
 }
 
 export default Reflow
