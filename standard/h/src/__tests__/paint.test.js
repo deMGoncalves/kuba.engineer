934b3553a1f95b6f07980768cb6bd10d0bb7f1f1
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 17:59:40 -0300

    test(h.paint): vincula na instancia o contrato flow

--- standard/h/src/__tests__/paint.test.js
+++ standard/h/src/__tests__/paint.test.js
@@ -2,5 +2,6 @@ describe('standard.h.paint', () => {
   test.todo('Executa a entidade quando for usado com o operador new')
   test.todo('Executa o component quando for usado como uma funcao')
   test.todo('Vincula na instancia o contrato rootElement')
+  test.todo('Vincula na instancia o contrato flow')
   test.todo('Vincula na instancia o contrato reflow')
 })
