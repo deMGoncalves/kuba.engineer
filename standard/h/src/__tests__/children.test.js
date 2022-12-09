d26f7b61a4be0c70ec81d004d03061220c91da18
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:31:53 -0300

    test(h.children): metodo estatico create mapeia todos os filhos validos

--- standard/h/src/__tests__/children.test.js
+++ standard/h/src/__tests__/children.test.js
@@ -4,4 +4,5 @@ describe('standard.h.children', () => {
   test.todo('Contrato reflow.replace troca um filho por outro novo filho')
   test.todo('Contrato render.flow adiciona todos os filhos no parent')
   test.todo('Contrato repaint.reflow executa o processo com o novo chldren')
+  test.todo('Metodo estatico create mapeia todos os filhos validos')
 })
