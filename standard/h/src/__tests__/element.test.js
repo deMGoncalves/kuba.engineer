abc55ece024c20085fb56aa11d3fe5f29124d277
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 14:48:31 -0300

    test(h.element): define ou altera um atributo no node

--- standard/h/src/__tests__/element.test.js
+++ standard/h/src/__tests__/element.test.js
@@ -28,6 +28,7 @@ describe('standard.h.element', () => {
   test.todo('Remove a className do node')
   test.todo('Remove o evento do node')
   test.todo('Troca um filho por outro novo filho')
+  test.todo('Define ou altera um atribuno no node')
 
   test('Contrato render.flow cria um HTMLElement e executa o mesmo contrato em suas dependencias', () => {
     const element = Element.create(nodeName, {}, [])
