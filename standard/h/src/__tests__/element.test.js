dc0141235931a12f337a5c8eda6b9b7c7344fabb
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 14:35:46 -0300

    test(h.element): remove o node do DOM

--- standard/h/src/__tests__/element.test.js
+++ standard/h/src/__tests__/element.test.js
@@ -23,6 +23,7 @@ describe('standard.h.element', () => {
   test.todo('Adiciona um novo node ao lado do node')
   test.todo('Adiciona todos os filhos no node')
   test.todo('Adiciona um novo filho no node')
+  test.todo('Remove o node do DOM')
 
   test('Contrato render.flow cria um HTMLElement e executa o mesmo contrato em suas dependencias', () => {
     const element = Element.create(nodeName, {}, [])
