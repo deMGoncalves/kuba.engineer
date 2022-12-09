ebcab8cff159769d564a7c5d467cf1a5ab9f0979
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:21:35 -0300

    test(h.attributes): contrato repaint.reflow executa o prrocesso com o novo attributes

--- standard/h/src/__tests__/attributes.test.js
+++ standard/h/src/__tests__/attributes.test.js
@@ -3,4 +3,5 @@ describe('standard.h.attributes', () => {
   test.todo('Contrato reflow.remove remove um atributo')
   test.todo('Contrato reflow.replace troca um atributo por outro novo atributo')
   test.todo('Contrato render.flow define todos os atributo no target')
+  test.todo('Contrato repaint.reflow executa o processo com o novo attributes')
 })
