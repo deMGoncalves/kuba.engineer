228bd9b5d68f4950cee899a638df3775756b579a
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 16:07:45 -0300

    test(h.classname): contrato repaint.reflow atualia o valor se o novo className for diferente

--- standard/h/src/__tests__/className.test.js
+++ standard/h/src/__tests__/className.test.js
@@ -1,4 +1,5 @@
 describe('standard.h.className', () => {
   test.todo('Atributo value retorna o valor do className do target')
   test.todo('Contrato render.flow define o className se o value for truthy')
+  test.todo('Contrato repaint.reflow atualiza o valor se o novo className for diferente')
 })
