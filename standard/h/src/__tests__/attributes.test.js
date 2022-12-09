922936240dc005dccfe3dc87c66f0560dc12a114
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:22:32 -0300

    test(h.attributes): metodo estatico create mapeia todos os atributos validos

--- standard/h/src/__tests__/attributes.test.js
+++ standard/h/src/__tests__/attributes.test.js
@@ -4,4 +4,5 @@ describe('standard.h.attributes', () => {
   test.todo('Contrato reflow.replace troca um atributo por outro novo atributo')
   test.todo('Contrato render.flow define todos os atributo no target')
   test.todo('Contrato repaint.reflow executa o processo com o novo attributes')
+  test.todo('Metodo estatico create mapeia todos os atributos validos')
 })
