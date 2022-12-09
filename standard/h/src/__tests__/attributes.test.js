506a565faa99d5973d828c479f090cd4130e5a9f
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:19:05 -0300

    test(h.attributes): contrato reflow.replace troca um atributo por outro novo atributo

--- standard/h/src/__tests__/attributes.test.js
+++ standard/h/src/__tests__/attributes.test.js
@@ -1,4 +1,5 @@
 describe('standard.h.attributes', () => {
   test.todo('Contrato reflow.add adiciona um novo atributo')
   test.todo('Contrato reflow.remove remove um atributo')
+  test.todo('Contrato reflow.replace troca um atributo por outro novo atributo')
 })
