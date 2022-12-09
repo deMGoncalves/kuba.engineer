effc2d0cf8ef949e6534f3b71f4621a6c0f163bd
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 13:27:58 -0300

    test(h.fragment): remove executa o metodo remove em todos os filhos

--- standard/h/src/__tests__/fragment.test.js
+++ standard/h/src/__tests__/fragment.test.js
@@ -12,6 +12,8 @@ describe('standard.h.fragment', () => {
 
   test.todo('Atributo children eh uma referencia a instancia de Children')
   test.todo('Os metodos appendChild e after executa o metodo after do ultimo child')
+  test.todo('Append renderiza todos os filhos e adiciona todos os filhos no node')
+  test.todo('Remove executa o metodo remove de todos os filhos')
 
   test('Contrato render.flow cria um DocumentFragment', () => {
     const node = fragment[render.flow]()
