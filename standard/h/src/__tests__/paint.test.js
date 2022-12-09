69c07382b3d3bd99b49ac416ead623f3a41f0b43
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 17:50:02 -0300

    test(h.paint): este describe nao precisa pular

--- standard/h/src/__tests__/paint.test.js
+++ standard/h/src/__tests__/paint.test.js
@@ -1,4 +1,4 @@
-describe.skipe('standard.h.paint', () => {
+describe('standard.h.paint', () => {
   test.todo('Executa a entidade quando for usado com o operador new')
   test.todo('Executa o component quando for usado como uma funcao')
   test.todo('Vincula na instancia o contrato rootElement')
