a3459e44f4b467f8a33cc92d5bf2d1ec9b1dd3c2
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:29:49 -0300

    test(h.children): contrato render.flow adiciona todos os filhos no parent

--- standard/h/src/__tests__/children.test.js
+++ standard/h/src/__tests__/children.test.js
@@ -2,4 +2,5 @@ describe('standard.h.children', () => {
   test.todo('Contrato reflow.add adiciona um novo filho')
   test.todo('Contrato reflow.remove remove um filho')
   test.todo('Contrato reflow.replace troca um filho por outro novo filho')
+  test.todo('Contrato render.flow adiciona todos os filhos no parent')
 })
