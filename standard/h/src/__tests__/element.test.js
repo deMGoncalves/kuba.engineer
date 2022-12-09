1a45b6a6798083ca924bdd07ff00a4e64ef40f67
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 14:02:55 -0300

    test(h.element): contrato render.flow cria um HTMLElement executa o contrato em suas dependencias

--- standard/h/src/__tests__/element.test.js
+++ standard/h/src/__tests__/element.test.js
@@ -14,15 +14,7 @@ describe('standard.h.element', () => {
     jest.clearAllMocks()
   })
 
-  test('Metodo estatico is valida se o nodeName eh um Element', () => {
-    expect(Element.is(nodeName)).toBeTruthy()
-  })
-
-  test('Metodo estatico create cria uma instancia de Element', () => {
-    expect(Element.create(nodeName, {}, [])).toBeInstanceOf(Element)
-  })
-
-  test('Cria um HTMLElement', () => {
+  test('Contrato render.flow cria um HTMLElement e executa o mesmo contrato em suas dependencias', () => {
     const element = Element.create(nodeName, {}, [])
     const node = element[render.flow]()
 
@@ -31,5 +23,13 @@ describe('standard.h.element', () => {
 
     expect(createElement).toHaveBeenCalledTimes(1)
     expect(createElement).toHaveBeenCalledWith(nodeName, { is: {} })
+  })
+
+  test('Metodo estatico is valida se o nodeName eh um Element', () => {
+    expect(Element.is(nodeName)).toBeTruthy()
+  })
+
+  test('Metodo estatico create cria uma instancia de Element', () => {
+    expect(Element.create(nodeName, {}, [])).toBeInstanceOf(Element)
   })
 })
