b4d97b35e580a0f06325f1a3d94e4301287ed171
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 18:00:54 -0300

    test(h.paint): vincula no ast o contrato paint.instance

--- standard/h/src/__tests__/paint.test.js
+++ standard/h/src/__tests__/paint.test.js
@@ -1,7 +1,8 @@
 describe('standard.h.paint', () => {
   test.todo('Executa a entidade quando for usado com o operador new')
   test.todo('Executa o component quando for usado como uma funcao')
-  test.todo('Vincula na instancia o contrato rootElement')
-  test.todo('Vincula na instancia o contrato flow')
-  test.todo('Vincula na instancia o contrato reflow')
+  test.todo('Vincula na instancia o contrato paint.rootElement')
+  test.todo('Vincula na instancia o contrato paint.flow')
+  test.todo('Vincula na instancia o contrato paint.reflow')
+  test.todo('Vincula no ast o contrato paint.instance')
 })
