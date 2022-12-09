3aab1c3b58423b198c7309f875d118870cb7f330
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 10:18:50 -0300

    test(h.render): padronizacao dos casos de tests

--- standard/h/src/__tests__/render.test.js
+++ standard/h/src/__tests__/render.test.js
@@ -1,9 +1,12 @@
 import render from '../render'
 
 describe('standard.h.render', () => {
+  let append
   let element
 
   beforeEach(() => {
+    append = jest.spyOn(document.body, 'append')
+
     element = {
       [render.flow]: jest.fn().mockReturnValue(HTMLElement)
     }
@@ -14,12 +17,9 @@ describe('standard.h.render', () => {
   })
 
   test('Apenda o HTMLElement retornado do contrato render.flow no element', () => {
-    const append = jest.spyOn(document.body, 'append')
-
     render(document.body, element)
 
     expect(element[render.flow]).toHaveBeenCalledTimes(1)
-
     expect(append).toHaveBeenCalledTimes(1)
     expect(append).toHaveBeenCalledWith(HTMLElement)
   })
