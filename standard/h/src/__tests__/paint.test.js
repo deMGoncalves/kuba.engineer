a25f9311c190f39117f0c806098cb3cdefc60dba
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 17:32:59 -0300

    test(h.paint): vincula na instancia o contrato rootElement

--- standard/h/src/__tests__/paint.test.js
+++ standard/h/src/__tests__/paint.test.js
@@ -1,4 +1,5 @@
 describe.skipe('standard.h.paint', () => {
   test.todo('Executa a entidade quando for usado com o operador new')
   test.todo('Executa o component quando for usado como uma funcao')
+  test.todo('Vincula na instancia o contrato rootElement')
 })
