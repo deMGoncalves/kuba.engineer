cb005ce87c315d1649ba1de1d5f61a5a92b4e698
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 15:30:53 -0300

    test(h.children): contrato repaint.reflow executa o proceso com o novo children

--- standard/h/src/__tests__/children.test.js
+++ standard/h/src/__tests__/children.test.js
@@ -3,4 +3,5 @@ describe('standard.h.children', () => {
   test.todo('Contrato reflow.remove remove um filho')
   test.todo('Contrato reflow.replace troca um filho por outro novo filho')
   test.todo('Contrato render.flow adiciona todos os filhos no parent')
+  test.todo('Contrato repaint.reflow executa o processo com o novo chldren')
 })
