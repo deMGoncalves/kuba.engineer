32911550c218596b5f0fdae73b3419f424d08f1e
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-09 16:06:13 -0300

    test(h.classname): contrato render.flow define o className se o valor for truthy

--- standard/h/src/__tests__/className.test.js
+++ standard/h/src/__tests__/className.test.js
@@ -1,3 +1,4 @@
 describe('standard.h.className', () => {
   test.todo('Atributo value retorna o valor do className do target')
+  test.todo('Contrato render.flow define o className se o value for truthy')
 })
