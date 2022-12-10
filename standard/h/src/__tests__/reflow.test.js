83db18b398943e88ea3b971cca3ccb2fd7e4a0f2
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 13:58:56 -0300

    test(h.reflow): quando somente o real existir executa o contrato reflow.remove no parent

--- standard/h/src/__tests__/reflow.test.js
+++ standard/h/src/__tests__/reflow.test.js
@@ -1,3 +1,4 @@
 describe('standard.h.reflow', () => {
   test.todo('Quando somente o virtual existir executa o contrato reflow.add no parent')
+  test.todo('Quando somente o real existir executa o contrato reflow.remove no parent')
 })
