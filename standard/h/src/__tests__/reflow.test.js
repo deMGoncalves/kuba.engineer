67a0cf39b89953b2cabf676a2133199f30b9a8ff
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 14:00:37 -0300

    test(h.reflow): quando o real e virtual forem diferentes executa o contrato reflow.replace no parent

--- standard/h/src/__tests__/reflow.test.js
+++ standard/h/src/__tests__/reflow.test.js
@@ -1,4 +1,5 @@
 describe('standard.h.reflow', () => {
   test.todo('Quando somente o virtual existir executa o contrato reflow.add no parent')
   test.todo('Quando somente o real existir executa o contrato reflow.remove no parent')
+  test.todo('Quando real e virtual forem diferentes executa o contrato reflow.repalce no parent')
 })
