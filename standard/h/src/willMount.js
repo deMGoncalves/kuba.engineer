2d3e730cc5164417afc2d30fef15a3b43847824b
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 15:16:39 -0300

    refactor(h.willmount): usando o setimmediate para executar o mais breve possivel a funcao referencia

--- standard/h/src/willMount.js
+++ standard/h/src/willMount.js
@@ -1,5 +1,6 @@
 import magic from '@kuba/magic'
 import paint from './paint'
+import setImmediate from '@kuba/setimmediate'
 
 function willMount (_target, _prop, descriptor) {
   const event = (this?.event ?? magic.willMount)
@@ -7,7 +8,7 @@ function willMount (_target, _prop, descriptor) {
 
   Object.assign(descriptor, {
     value () {
-      setTimeout(() => this[paint.instance]?.[event]?.())
+      setImmediate(() => this[paint.instance]?.[event]?.())
       return Reflect.apply(next, this, arguments)
     },
     writable: true
