f26a089de8e7a32a201bb300046ff67bd4927478
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 14:39:02 -0300

    test(h.text): remove o node do DOM

--- standard/h/src/__tests__/text.test.js
+++ standard/h/src/__tests__/text.test.js
@@ -1,5 +1,5 @@
 describe('standard.h.text', () => {
   test.todo('Atributo content retorna o valor do texto')
-  test.todo('Metodo after adiciona um node ao lado do texto')
+  test.todo('Adiciona um novo node ao lado do text')
   test.todo('Remove o node do DOM')
 })
