16c990eefdd18eebd1ce54b5c40f6fae6ab45018
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 20:33:30 -0300

    test(h.classname): atributo key retorna a chave para definir o atributo no node

--- standard/h/src/__tests__/className.test.js
+++ standard/h/src/__tests__/className.test.js
@@ -1,4 +1,5 @@
 describe('standard.h.className', () => {
+  test.todo('Atributo key retorna a chave para definir o atributo no node')
   test.todo('Atributo value retorna o valor do className do target')
   test.todo('Contrato render.flow define o className se o value for truthy')
   test.todo('Contrato repaint.reflow atualiza o valor se o novo className for diferente')
