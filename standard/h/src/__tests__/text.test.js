f643778f4d5260a87363c28fe5710de8a0d808fd
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 14:40:06 -0300

    test(h.text): contrato reflow.different valida se o novo text eh diferente

--- standard/h/src/__tests__/text.test.js
+++ standard/h/src/__tests__/text.test.js
@@ -2,4 +2,5 @@ describe('standard.h.text', () => {
   test.todo('Atributo content retorna o valor do texto')
   test.todo('Adiciona um novo node ao lado do text')
   test.todo('Remove o node do DOM')
+  test.todo('Contrato reflow.different valida se o novo text eh diferente')
 })
