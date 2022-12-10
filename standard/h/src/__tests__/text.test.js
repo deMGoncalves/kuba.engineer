053052b34437b1469256239e0be25e8b4fafc675
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-10 14:41:32 -0300

    test(h.text): contrato render.flow cria o node

--- standard/h/src/__tests__/text.test.js
+++ standard/h/src/__tests__/text.test.js
@@ -3,4 +3,5 @@ describe('standard.h.text', () => {
   test.todo('Adiciona um novo node ao lado do text')
   test.todo('Remove o node do DOM')
   test.todo('Contrato reflow.different valida se o novo text eh diferente')
+  test.todo('Contrato render.flow cria o node')
 })
