50411fc399a4da830911e5534543c90d9740b54d
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-11 16:07:34 -0300

    test(revoke): quando acessado um metodo sera vinculado a instancia

--- standard/revoke/src/__test__/revoke.test.js
+++ standard/revoke/src/__test__/revoke.test.js
@@ -1,3 +1,4 @@
 describe('standard.revoke', () => {
   jest.todo('Referencia da class eh instanciado apos o primeiro acesso')
+  jest.todo('Quando acessado um metodo sera vinculado a instancia')
 })
