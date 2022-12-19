71d62e07412477f7bac0080bc458e708932370bb
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-19 13:43:58 -0300

    chore(f): expondo os modulos arity e zip

--- standard/f/src/index.js
+++ standard/f/src/index.js
@@ -1,2 +1,5 @@
 export { __ } from './curry'
+export { default as arity } from './arity'
 export { default as curry } from './curry'
+export { default as magic } from './magic'
+export { default as zip } from './zip'
