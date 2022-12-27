991771922a2a0363cb3cf3e1d76d256bc6542b99
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-27 17:05:49 -0300

    test(show): regras de que deverm ser validados

--- directive/show/src/__tests__/show.test.js
+++ directive/show/src/__tests__/show.test.js
@@ -1,4 +1,4 @@
-describe('standard.show', () => {
+describe('directive.show', () => {
   test.todo('Retorna o children quando a propriedade data-when for truthy')
   test.todo('Retorna um array vazio quando a propriedade data-when for falsy')
 })
