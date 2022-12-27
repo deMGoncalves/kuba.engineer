f60fc349053d2947b62f3fde11767ddd5bceee33
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-27 17:08:05 -0300

    test(zone): teste do uso do component

--- pages/home/src/home.jsx
+++ pages/home/src/home.jsx
@@ -1,8 +1,11 @@
 import h from '@kuba/h'
+import Zone from '@kuba/zone'
 
 function home () {
   return (
-    <h1>kuba.engineer</h1>
+    <Zone>
+      <h1>kuba.engineer</h1>
+    </Zone>
   )
 }
 
