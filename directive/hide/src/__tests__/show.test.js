7456004ce166d4b7f5378b699142e564cc12be2d
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-27 17:05:35 -0300

    test(hide): regras de que deverm ser validados

--- directive/hide/src/__tests__/show.test.js
+++ directive/hide/src/__tests__/show.test.js
@@ -1,4 +1,4 @@
-describe('standard.hide', () => {
+describe('directive.hide', () => {
   test.todo('Retorna o children quando a propriedade data-when for falsy')
   test.todo('Retorna um array vazio quando a propriedade data-when for truthy')
 })
