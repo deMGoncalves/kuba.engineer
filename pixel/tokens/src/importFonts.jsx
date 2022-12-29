06b5da477d7dfe216b4af91b7ac190ca3c2e08e9
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-29 18:30:33 -0300

    feat(tokens): configuracao das variaveis para os styleds

---
+++ pixel/tokens/src/importFonts.jsx
@@ -0,0 +1,9 @@
+import h, { Fragment, render } from '@kuba/h'
+
+render(
+  document.head,
+  <>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap' />
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;500;700&display=swap' />
+  </>
+)
