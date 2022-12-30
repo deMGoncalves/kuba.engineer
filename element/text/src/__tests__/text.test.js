5f1b50d27d62b9f127c793f007f8d5abd4c3918b
deMGoncalves <cleber.engineer@hotmail.com>
2022-12-30 12:06:38 -0300

    test(text): contratos dos testes que devem ser escrritos

---
+++ element/text/src/__tests__/text.test.js
@@ -0,0 +1,6 @@
+describe('element.text', () => {
+  test.todo('Cria dinamicamente um elemento html')
+  test.todo('Define a cor e seu modificado com base nos atributos')
+  test.todo('Define o tamanho com base no atriuto')
+  test.todo('Define o peso da font com base no atributo')
+})
