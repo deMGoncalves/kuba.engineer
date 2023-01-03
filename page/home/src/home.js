52d234b220467296f55270112f80db1d0d620033
deMGoncalves <cleber.engineer@hotmail.com>
2023-01-03 11:54:27 -0300

    refactor(home): nao tera dados estruturado

--- page/home/src/home.js
+++ page/home/src/home.js
@@ -1,11 +1,8 @@
 import { paint } from '@kuba/h'
 import { setDescription, setTitle } from '@kuba/markup'
 import component from './component'
-import data from './data'
-import jsonld from '@kuba/jsonld'
 
 @paint(component)
-@jsonld(data)
 class Home {
   #description = 'Simples, pequeno e imperfeito. Uma visão além da programação.'
   #title = 'kuba'
@@ -13,22 +10,6 @@ class Home {
   constructor () {
     setTitle(this.#title)
     setDescription(this.#description)
-  }
-
-  [data.description] () {
-    return this.#description
-  }
-
-  [data.language] () {
-    return 'pt-BR'
-  }
-
-  [data.title] () {
-    return this.#title
-  }
-
-  [data.url] () {
-    return `https://${location.hostname}`
   }
 }
 
